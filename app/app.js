342dae3c37811990dba1062d876d1d8ca9b174a5
realjavedansari <134133786+realjavedansari@users.noreply.github.com>
2023-05-27 14:25:26 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 // TODO: Put go into a config.js
 // But how to include a file from local?
 
-var GETH_HOSTNAME	= "localhost";	// put your IP address!
+var GETH_HOSTNAME	= "https://rpc-dataseed.bitsdeal.com";	// put your IP address!
 var APP_HOSTNAME 	= "See package.json --> scripts --> start: Change 'localhost'!!!";
 
 var GETH_RPCPORT  	= 8545; 		// for geth --rpcport GETH_RPCPORT
@@ -11,7 +11,7 @@ var APP_PORT 		= "See package.json --> scripts --> start: Perhaps change '8000'"
 
 // this is creating the corrected geth command
 var WL=window.location;
-var geth_command	= "geth --rpc --rpcaddr "+ GETH_HOSTNAME + " --rpcport " + GETH_RPCPORT +'\
+var geth_command	= "geth --rpc --rpcaddr "+ GETH_HOSTNAME + '\
  --rpcapi "web3,eth" ' + ' --rpccorsdomain "' + WL.protocol +"//" + WL.host + '"';
 
 ////////////////////////////////////////////////////
